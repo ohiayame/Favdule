6908a37c74cb81476e79468759d9c026a54a417c
ohiayame <0819aym001@gmail.com>
2025-10-07 21:22:50 +0900

    refact

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -3,13 +3,13 @@ import api from "./apiClient";
 // 사용자 정보
 export const getUser = async (google_id) => {
   const res = await api.get(`/users/${google_id}`);
-  console.log("user", res.data);
+  // console.log("user", res.data);
   return res.data;
 };
 
 // 로그인
 export const getLogin = async (token) => {
-  console.log(token);
+  // console.log(token);
   const res = await api.post(`/users`, { token });
   return res.data;
 };
