cb64f8b806914910bb8608d43bd67a4d1426b120
ohiayame <0819aym001@gmail.com>
2025-11-24 00:32:26 +0900

    refact:  store 추가

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,8 +1,7 @@
 import Layout from "@/layouts/Layout";
 import DayContainer from "./DayContainer";
 import { useState, Fragment } from "react";
-import { getVideos } from "@/api/groupsApi";
-import { useAuthStore } from "@/store/auth";
+import { useVideosStore } from "@/store/video";
 
 import { useTheme, useMediaQuery } from "@mui/material";
 import Grid from "@mui/material/Grid";
@@ -14,8 +13,7 @@ function Home() {
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   const [groupId, setGroupId] = useState(null);
-  const user = useAuthStore((state) => state.user);
-  const groupData = JSON.parse(localStorage.getItem("groupData"));
+  const { getVideos } = useVideosStore();
   const dayList = ["yesterday", "today", "tomorrow"];
 
   // 영상 정보 저장
@@ -27,46 +25,16 @@ function Home() {
 
   // 채널의 영상 나눠서 조회 후 셋 (어제, 오늘, 내일)
   const fetchVideos = async (g_id = groupId) => {
-    // console.log("fetchVideos", groupData[g_id]);
-    let res = null;
-    if (user) {
-      res = await getVideos(g_id, null);
-    } else {
-      res = await getVideos(null, groupData[g_id]);
-    }
-    const { yesterday, today, tomorrow } = res;
-    // console.log("yesterday", g_id);
-
-    // TEST =========================================================
-    // localStorage.setItem(
-    //   "videos",
-    //   JSON.stringify({ yesterday, today, tomorrow })
-    // );
-    // const storedVideos = JSON.parse(localStorage.getItem("videos"));
-    // const { yesterday: y, today: t, tomorrow: m } = storedVideos;
-    // setVideos({
-    //   yesterday: y,
-    //   today: t,
-    //   tomorrow: m,
-    // });
-    // ===============================================================
-    setVideos({
-      yesterday: yesterday,
-      today: today,
-      tomorrow: tomorrow,
-    });
+    // store로 관리 새로고침하면 다시 로딩됨
+    const { yesterday, today, tomorrow } = await getVideos(g_id);
+    setVideos({ yesterday, today, tomorrow });
   };
 
   // HomeFiltering에서 groupId 선택후 셋 -> 채널 조회
   const handleGroup = (g_id) => {
     setGroupId(g_id);
-    // console.log(g_id);
-
     fetchVideos(g_id);
   };
-  // useEffect(() => {
-  //   fetchVideos();
-  // }, [user]);
 
   return (
     <Layout title="Home" groupId={groupId} onFilterChange={handleGroup}>
